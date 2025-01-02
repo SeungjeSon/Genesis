c85d7327b4b327c2793798b6b2ad53cb6f15d4d3
SJ <seungje0927@gmail.com>
2025-01-03 00:54:45 +0900

    manipulator_test1 + genesis 가이드 설정 추가

--- SON/tutorial/manipulator_test1.py
+++ SON/tutorial/manipulator_test1.py
@@ -1,18 +1,20 @@
 import genesis as gs
 
 def run_sim(scene, enable_vis):
-    for i in range(1000):
+    for i in range(500):
         scene.step()
     if enable_vis:
-        scene.viewr.stop()
+        scene.viewer.stop()
 
 gs.init(backend=gs.metal) # mac gpu사용하기 위해 gs.metal
 scene = gs.Scene(
     sim_options=gs.options.SimOptions(),
     viewer_options=gs.options.ViewerOptions(
+        res = (1280, 960),      # default : None --> 4:3 창으로 자동 설정됨
         camera_pos=(3.5, 0.0, 2.5),
         camera_lookat=(0.0, 0.0, 0.5),
         camera_fov=40,
+        max_FPS = 60,           # 지정 안하면 자동 FPS 설정됨
     ),
     show_viewer=True,
     rigid_options=gs.options.RigidOptions(
@@ -22,7 +24,14 @@ scene = gs.Scene(
 )
 
 plane = scene.add_entity(gs.morphs.Plane())
-franka = scene.add_entity(gs.morphs.MJCF(file='xml/franka_emika_panda/panda.xml'), )
+franka = scene.add_entity(gs.morphs.MJCF(
+    file='xml/franka_emika_panda/panda.xml',
+    pos = (0, 0, 0),
+    euler = (0, 0, 90), # we follow scipy's extrinsic x-y-z rotation convention, in degrees,
+        # quat  = (1.0, 0.0, 0.0, 0.0), # we use w-x-y-z convention for quaternions,
+    scale = 1.0,
+    ),
+)
 
 scene.build()
 gs.tools.run_in_another_thread(
